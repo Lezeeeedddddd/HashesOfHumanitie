47901841ffa03b7b1d95c98efdaedb2ac11fc4d5
Elie Zelaznik <elie.zelaznik@epita.fr>
2025-12-15 20:28:08 +0100

    Correction du bug a l'ouverture du jeu

--- Games/Resssource/tileset/vile.tsx
+++ Games/Resssource/tileset/vile.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="roguelikeCity_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
- <image source="../../../../../roguelikeCity_transparent.png" trans="ff00ff" width="628" height="475"/>
+ <transformations hflip="0" vflip="0" rotate="1" preferuntransformed="0"/>
+ <image source="roguelikeCity_transparent.jpg" trans="ff00ff" width="628" height="475"/>
 </tileset>
